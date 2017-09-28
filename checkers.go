58c414284c473f9e43585a0febf02196b71dd9a5
Chris Griffith <chris.griffith@rackspace.com>
2017-09-28 00:08:47 -0500

    adding some skippinig logic if a player has no moves

--- checkers.go
+++ checkers.go
@@ -10,6 +10,8 @@ import (
 )
 
 var playing bool
+var playerOneStuck bool
+var playerTwoStuck bool
 
 func GetPossiblePos(player int) (int, int) {
 	vert := -1
@@ -38,7 +40,7 @@ func ValidMove(player int, mov1 [2]int, mov2 [2]int, capOnly bool, noLog bool) b
 		if ! noLog {
 			fmt.Println("Starting posistion invalid", board[mov1[0]][mov1[1]])
 		}
-			return false
+		return false
 	}
 	if board[mov2[0]][mov2[1]] != 3 {
 		if ! noLog {
@@ -46,7 +48,6 @@ func ValidMove(player int, mov1 [2]int, mov2 [2]int, capOnly bool, noLog bool) b
 		}
 		return false
 	}
-
 
 	if CaptureChecks(player, mov1, mov2, capOnly, enemy, false) {
 		return true
@@ -71,11 +72,11 @@ func CaptureChecks(player int, mov1 [2]int, mov2 [2]int, capOnly bool, enemy int
 
 	if mov2[0]-mov1[0] == vertCap {
 		if hoz == -2 && OnBoard(mov1[0]+vert, mov1[1]+1) {
-			if board[mov1[0]+vert ][mov1[1]+1] == enemy || board[mov1[0]+vert ][mov1[1]+1] == enemy + 6 {
+			if board[mov1[0]+vert ][mov1[1]+1] == enemy || board[mov1[0]+vert ][mov1[1]+1] == enemy+6 {
 				return true
 			}
 		} else if hoz == 2 && OnBoard(mov1[0]+vert, mov1[1]-1) {
-			if board[mov1[0]+vert ][mov1[1]-1] == enemy || board[mov1[0]+vert ][mov1[1]-1] == enemy + 6{
+			if board[mov1[0]+vert ][mov1[1]-1] == enemy || board[mov1[0]+vert ][mov1[1]-1] == enemy+6 {
 				return true
 			}
 		}
@@ -83,8 +84,69 @@ func CaptureChecks(player int, mov1 [2]int, mov2 [2]int, capOnly bool, enemy int
 	return false
 }
 
+func ValidMoves(player int) [][2][2]int {
+	var valids [][2][2]int
+	vert, vertCap := GetPossiblePos(player)
+
+	for i, row := range board {
+		for j, element := range row {
+			if element != player && element != player+6 {
+				continue
+			}
+			checkMoves := [4][2]int{
+				{i + vert, j + 1},
+				{i + vert, j - 1},
+				{i + vertCap, j + 2},
+				{i + vertCap, j - 2},
+			}
+			currentPos := [2]int{i, j}
+			for _, move := range checkMoves {
+				if ValidMove(player, currentPos, move, false, true) {
+					valids = append(valids, [2][2]int{currentPos, move})
+				}
+			}
+
+			if element == player+6 {
+				kingMoves := [4][2]int{
+					{i - vert, j + 1},
+					{i - vert, j - 1},
+					{i - vertCap, j + 2},
+					{i - vertCap, j - 2},
+				}
+				for _, move := range kingMoves {
+					if ValidMove(player, currentPos, move, false, true) {
+						valids = append(valids, [2][2]int{currentPos, move})
+					}
+				}
+			}
+		}
+	}
+	return valids
+}
+
 func PlayerTurn(player int, capOnly bool, lastPos [2]int) {
 	PrintBoard()
+	if len(ValidMoves(player)) == 0 {
+		fmt.Println("No moves for you, bad luck!")
+		if player == 1 {
+			playerOneStuck = true
+		} else {
+			playerTwoStuck = true
+		}
+		if playerOneStuck && playerTwoStuck {
+			fmt.Println("It's a draw!")
+			playing = false
+			return
+		}
+
+	} else {
+		if player == 1 {
+			playerOneStuck = false
+		} else {
+			playerTwoStuck = false
+		}
+	}
+
 	reader := bufio.NewReader(os.Stdin)
 
 	rep := "X"
@@ -98,14 +160,6 @@ func PlayerTurn(player int, capOnly bool, lastPos [2]int) {
 	if strings.HasPrefix(strings.ToLower(text), "quit") {
 		playing = false
 		fmt.Println("Quitting...")
-		return
-	} else if strings.HasPrefix(strings.ToLower(text), "skip") {
-		if capOnly {
-			fmt.Println("Womp womp, you have to take the capture")
-			PlayerTurn(player, capOnly, lastPos)
-			return
-		}
-		fmt.Println("Skipping turn")
 		return
 	}
 
@@ -180,14 +234,14 @@ func PlayerTurn(player int, capOnly bool, lastPos [2]int) {
 }
 
 func main() {
-
+	playerOneStuck = false
+	playerTwoStuck = false
 	fmt.Println("Welcome to GO Checkers!\n")
 	fmt.Println("You play by specifiying which peice to move, and the posistion to move it too")
 	fmt.Println("You can string captures, the game will let you know if you must take the next capture")
 	fmt.Println("You can also 'skip' or 'quit'")
 	fmt.Println("\nHave fun!\n")
 	PopulateNewBoard()
-
 	playing = true
 	for playing {
 		PlayerTurn(1, false, [2]int{0, 0})
